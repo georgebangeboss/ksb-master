68b7798d282d92b39d4e45e510500cac165776f5
Bange boss <bangegeorgeogeto@gmail.com>
2022-03-08 15:46:56 +0300

    added static files

--- ksb/settings.py
+++ ksb/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT = BASE_DIR / "media"
