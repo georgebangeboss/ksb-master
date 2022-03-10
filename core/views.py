87b6999c82d44ddd3ddb2f5c6e090e656fe89f47
Bange boss <bangegeorgeogeto@gmail.com>
2022-03-10 08:50:52 +0300

    images add

--- core/views.py
+++ core/views.py
@@ -62,7 +62,7 @@ class DailyWorkSheetCreateAPIView(generics.CreateAPIView):
                 "New WorkSheet",
                 "A new work sheet has been created now",
                 settings.EMAIL_HOST_USER,
-                ["tonnymaishaogeto@gmail.com"],
+                ["paul.musau@ksb.com"],
                 ["frankogetomitema@gmail.com"],
             )
             pdf_mime.add_header("Content-Disposition", "attachment", filename=file_name)
