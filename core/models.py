439a402d2f86b34f1ac1004a183a698b61470121
Bange boss <bangegeorgeogeto@gmail.com>
2022-03-10 18:50:52 +0300

    b64

--- core/models.py
+++ core/models.py
@@ -67,6 +67,7 @@ class DailyWorkSheet(models.Model):
     def get_worksheet_image(self):
         with open(self.work_sheet_images.path, "rb") as image_file:
             encode_string = base64.b64decode(image_file.read())
+        print(encode_string)
         return encode_string
 
     class Meta:
