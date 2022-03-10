cad3fed9b623f335bb4f01498e70f1cbadf27d8c
Bange boss <bangegeorgeogeto@gmail.com>
2022-03-10 18:03:46 +0300

    gmail tweak

--- core/views.py
+++ core/views.py
@@ -63,7 +63,7 @@ class DailyWorkSheetCreateAPIView(generics.CreateAPIView):
                 "A new work sheet has been created now",
                 settings.EMAIL_HOST_USER,
                 ["tonnymaishaogeto@ksb.com"],
-                [],
+                ["frankogeto@gmail.com"],
             )
             pdf_mime.add_header("Content-Disposition", "attachment", filename=file_name)
 
